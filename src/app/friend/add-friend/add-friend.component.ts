828b13c5da7ca9701d14fc993fae59e92be3bd38
Nanra Sukedy <sukedynanra@gmail.com>
2018-09-13 13:29:54 +0700

    Modify Add Friend Components

--- src/app/friend/add-friend/add-friend.component.ts
+++ src/app/friend/add-friend/add-friend.component.ts
@@ -1,5 +1,5 @@
 import { Friend } from './../friend.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-add-friend',
@@ -10,12 +10,15 @@ export class AddFriendComponent implements OnInit {
 
   // Variabel objek baru inherence dari class Friend yang dari modul friend.modul.ts
   inputInfo: Friend = new Friend ('', '', '');
+  @ViewChild('inputContact') inputContact: ElementRef;
 
   constructor() { }
 
-  onAddFriend() {
+  onAddFriend(inputEmail: HTMLInputElement) {
     console.log('Add Friend');
     console.log(this.inputInfo);
+    console.log('Email : ' + inputEmail.value);
+    console.log('Contact : ' + (this.inputContact.nativeElement.value));
   }
 
   ngOnInit() {
