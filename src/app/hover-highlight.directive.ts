446000e97367af9c47a6deecad092ae23f8660d0
Nanra Sukedy <sukedynanra@gmail.com>
2018-09-13 16:31:10 +0700

    Editing Hover Directive

--- src/app/hover-highlight.directive.ts
+++ src/app/hover-highlight.directive.ts
@@ -1,22 +1,34 @@
 import { Directive,
          ElementRef,
          Renderer2,
-         HostListener } from '@angular/core';
+         HostListener,
+         HostBinding,
+         Input } from '@angular/core';
 
 @Directive({
   selector: '[appHoverHighlight]'
 })
 export class HoverHighlightDirective {
 
+  @Input('appHoverHighlight') highlightColor: {background: string, text: string};
+
+  @HostBinding('style.color') textColor: string;
+  @HostBinding('style.font-weight') textFont: string;
+
   constructor( private elRef: ElementRef,
                private renderer: Renderer2) { }
 
   @HostListener('mouseenter') mouseOver(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'aqua');
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color',
+    this.highlightColor.background);
+    this.textColor = this.highlightColor.text;
+    this.textFont = 'bold';
   }
 
   @HostListener('mouseleave') mouseExit(eventData: Event) {
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.textColor = 'red';
+    this.textFont = 'normal';
   }
 
 }
