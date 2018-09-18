3728b057e315a5f671126d51ff8397a7986f86be
Nanra Sukedy <sukedynanra@gmail.com>
2018-09-18 09:33:34 +0700

    After Merge with Kang Yus

--- src/app/friend/friend.component.ts
+++ src/app/friend/friend.component.ts
@@ -15,17 +15,6 @@ export class FriendComponent implements OnInit {
   constructor(private friendService: FriendsService) { }
 
   ngOnInit() {
-<<<<<<< HEAD
-    console.log('Get Friendlist');
-    // this.friendList = this.friendService.getFriends();
-    this.friendService.loadFriends().subscribe((result: any) => {
-      console.log(result);
-      this.friendList = result.body;
-    },
-    error => {
-      console.log('Eroorr Gan');
-      console.log(error);
-=======
     // this.friendList = this.friendService.getFriends();
 
     this.friendService.loadFriends().subscribe(result => {
@@ -35,7 +24,6 @@ export class FriendComponent implements OnInit {
 
     this.friendService.friendAdded.subscribe(() => {
       this.friendList = this.friendService.getFriends();
->>>>>>> 81f3382d923a72cd59311e4df70cb2cd40a498d8
     });
 
     // this.friendService.friendAdded.subscribe(() => {
--- src/app/friend/friends.service.ts
+++ src/app/friend/friends.service.ts
@@ -6,21 +6,6 @@ import { map } from 'rxjs/operators';
 export class FriendsService {
   friendAdded = new EventEmitter<void>();
 
-<<<<<<< HEAD
-  private friendList: Friend[] = [];
-
-  constructor( private http: HttpClient) { }
-
-  loadFriends() {
-    return this.http.get('http://localhost:3000/api/friends', {observe: 'response'});
-    // .subscribe((response: Response) => {
-    //   let data = response.json();
-    //   this.friendList.push(data);
-    //   return data;
-    // },
-    // (error) => console.log(error)
-    // );
-=======
   // private friendList: Friend[] = [
   //   new Friend('Alfath Rajaya', 'alfath@email.com', 'Medan', '087265342')
   // ];
@@ -38,28 +23,11 @@ export class FriendsService {
     }, error => {
       console.log(error);
     }));
->>>>>>> 81f3382d923a72cd59311e4df70cb2cd40a498d8
   }
 
   addFriend(newFriendInfo) {
     // this.friendList.push(newFriendInfo);
     // this.friendAdded.emit();
-<<<<<<< HEAD
-    console.log('Friend List Awal');
-    console.log(this.friendList);
-    const headers = new HttpHeaders({'Content-Type': 'application/json'});
-    this.http.post('http://localhost:3000/api/friends', {info: newFriendInfo}, {headers: headers}).subscribe((
-      response: any) => {
-        const data = response;
-        this.friendList.push(data);
-        this.friendAdded.emit();
-        console.log('Response Beken');
-        console.log(data);
-
-      },
-      (error) => console.log(error)
-    );
-=======
     const headers = new HttpHeaders({'Content-Type': 'application/json'});
     this._http.post(`http://localhost:3000/api/friends`, {info: newFriendInfo}, {observe: 'response', headers: headers}).subscribe((response: any) => {
       console.log('masuk add :');
@@ -71,7 +39,6 @@ export class FriendsService {
       console.log(error);
 
     });
->>>>>>> 81f3382d923a72cd59311e4df70cb2cd40a498d8
   }
 
 
